135a632c4e351a5d62948e764b12006b256f6dab
Felipe Mamede <felipebrasilmv@gmail.com>
2024-12-09 21:27:27 -0300

    :tada: initial structure project and create splash screen

---
+++ nearby-app/Sources/Features/Coordinator/NearbyFlowController.swift
@@ -0,0 +1,25 @@
+//
+//  FlowCordinator.swift
+//  nearby-app
+//
+//  Created by Felipe Mamede on 09/12/24.
+//
+
+import Foundation
+import UIKit
+
+class NearbyFlowController {
+    private var navigationController: UINavigationController?
+    
+    public init() {
+        
+    }
+    
+    func start() -> UINavigationController? {
+        let contentView = SplashView()
+        let startViewController = SplashViewController(contentView: contentView)
+        self.navigationController = UINavigationController(rootViewController: startViewController)
+        
+        return navigationController
+    }
+}
